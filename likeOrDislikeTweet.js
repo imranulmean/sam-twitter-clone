9b4b28aa676f6bd41907138e77e7f14d2af1fad9
imranulmean <imranulhasan73@gmail.com>
2023-12-10 05:27:41 +0600

    like or dislike

--- likeOrDislikeTweet.js
+++ likeOrDislikeTweet.js
@@ -1,4 +1,4 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, UpdateItemCommand  } from "@aws-sdk/client-dynamodb";
 import { PutCommand, QueryCommand, UpdateCommand ,DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({});
@@ -21,7 +21,7 @@ export const handler = async (event) => {
   console.log(userId);
     /////////////////// Getting Tweets by UserId ///////////
     result= await updateTweet(userId, tweetId);
-    console.log(result);
+    console.log("showing result",result);
     let response = {
       statusCode: 200,
       'headers': {
@@ -36,23 +36,86 @@ export const handler = async (event) => {
 };
 
 const updateTweet = async (userId, tweetId) =>{
-    let command = new UpdateCommand({
+      // Check if userId is already in the likes array
+      const isLiked = false;  // Set this value based on your logic
+
+      // Define the update expression and attribute values based on whether to add or remove the userId
+      let updateExpression;
+      let expressionAttributeValues;
+
+      if (isLiked) {
+        // If already liked, remove userId from likes array
+        updateExpression = "REMOVE likes :userId";
+        expressionAttributeValues = { ":userId": { SS: [userId] } };
+      } else {
+        // If not liked, add userId to likes array
+        updateExpression = "ADD likes :userId";
+        expressionAttributeValues = { ":userId": { SS: [userId] } };
+      }
+
+      const updateParams = {
         TableName: "tweets",
         Key: {
-           userId:userId,
-          _id: tweetId
+          userId: { S: userId },
+          _id: { S: tweetId },
+        },
+        UpdateExpression: updateExpression,
+        ExpressionAttributeValues: expressionAttributeValues,
+        ReturnValues: "ALL_NEW",  // Adjust as needed
+      };
 
-        },
-       UpdateExpression: "set likes = list_append(likes, :newLike)",
-        ExpressionAttributeValues: {
-            ':newLike': [userId]
-        },
-        ReturnValues: "ALL_NEW",
-      });
+      const updateCommand =  new UpdateItemCommand(updateParams);
+      try {
+        return await docClient.send(updateCommand);
+      } catch (error) {
+        console.log(error);
+      }
     
-      const response = await docClient.send(command);
-      console.log(response);
-      return response;
 }
 
 await handler(event1);
+
+
+
+    // let command = new UpdateCommand({
+    //     TableName: "tweets",
+    //     Key: {
+    //        userId:userId,
+    //       _id: tweetId
+
+    //     },
+    //    UpdateExpression: "set likes = list_append(likes, :newLike)",
+    //     ExpressionAttributeValues: {
+    //         ':newLike': [userId]
+    //     },
+    //     ReturnValues: "ALL_NEW",
+    //   });
+    
+    //   const response = await docClient.send(command);
+    //   console.log(response);
+    //   return response;
+
+//////////////////////////Create new array if not exist
+
+
+  //   let command = new UpdateCommand({
+  //     TableName: "tweets",
+  //     Key: {
+  //         userId: userId,
+  //         _id: tweetId
+  //     },
+  //     UpdateExpression: "SET #likes = list_append(if_not_exists(#likes, :emptyList), :newLike)",
+  //     ExpressionAttributeNames: {
+  //         "#likes": "likes"
+  //     },
+  //     ExpressionAttributeValues: {
+  //         ":emptyList": [],
+  //         ":newLike": [userId]
+  //     },
+  //     ConditionExpression: "NOT contains(#likes, :userId)",
+  //     ReturnValues: "ALL_NEW",
+  // });
+  
+  // const response = await docClient.send(command);
+  // console.log(response);
+  // return response;
