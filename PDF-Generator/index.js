744524e1eb65bd2f8d46fed2028beb720b2a848a
Imranul Hasan <imranulhasan73@gmail.com>
2024-01-04 16:31:23 +0600

    pdf generator added

--- PDF-Generator/index.js
+++ PDF-Generator/index.js
@@ -1,37 +1,29 @@
 import express from 'express';
 import cors from 'cors';
-import generatePDF from './pdfGenerator.mjs';
+import puppeteer from 'puppeteer';
 
 const app= express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", async(req,res)=>{     
-    let response = {
-        statusCode: 200,
-        'headers': {
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': '*'
-        },    
-        body: JSON.stringify("Hello"),
-      };
-      res.send(response);
+
 });
 
 app.post('/generateResume', async (req, res) => {
-    try {
-      const resumeData = req.body;
-      const pdfBuffer = await generatePDF(resumeData);
-  
+
+      console.log(req.body);
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.setContent(req.body);
+
+      const pdfBuffer = await page.pdf();
+      await browser.close();
+
       res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', 'attachment; filename=resume.pdf');
+      res.setHeader('Content-Disposition', 'attachment; filename=generated.pdf');
       res.send(pdfBuffer);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+    });
 
 app.listen(3000,()=>{
     console.log("Server Started at 3000");
